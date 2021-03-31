96d2c4cc2559af6075c663b814ce2b650e056a4e
MUsoftware <musoftware@daum.net>
2021-03-31 22:49:55 +0900

    Add exception and stackTrace fields on ExcMsg

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,8 +1,10 @@
 class ExcMsg implements Exception {
   final String showingMessage;
   String debugMessage;
+  dynamic exception;
+  StackTrace stackTrace;
 
-  ExcMsg(this.showingMessage, {this.debugMessage});
+  ExcMsg(this.showingMessage, {this.debugMessage, this.exception, this.stackTrace});
 
   get debugMsg => this.debugMessage ?? this.showingMessage;
 }
